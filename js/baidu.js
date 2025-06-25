baacf3c88d009a518916dc1ea685fbd81f436aee
meineson <meineson@hotmail.com>
2025-06-25 13:36:31 +0800

    Site updated: 2025-06-25 13:36:31

---
+++ js/baidu.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?5668cb7e13223a3d281562fdf08e504b";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
